dbfb4ca14c70031a72dd90acf29e508c0cda6035
nayriz <john.aoussou@gmail.com>
2017-06-14 21:58:42 -0400

    Create matrix_ID_NN.py

--- matrix_ID_NN.py
+++ matrix_ID_NN.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  1 22:45:52 2017
-
-@author: john
-"""
 import numpy as np
 import random
 
@@ -33,12 +26,10 @@ yNot = np.zeros( (2,1) )
 yNot[0,0] = 1
 yNot[1,0] = 0
 
-# initialize the weights with random values
-w0 = random.uniform(-1,1)*10
-w1 = random.uniform(-1,1)*10
+# initialize w and populate it with random values  
 w = np.zeros( (2,1) )
-w[0,0] = w0
-w[1,0] = w1 
+w[0,0] = random.uniform(-1,1)*10
+w[1,0] = random.uniform(-1,1)*10
  
 # choose a gradient descent step
 alpha = .01;
@@ -46,7 +37,7 @@ alpha = .01;
 # chose the IDENTITY (Y = Yid) or NOT operator (Y = Ynot)
 y = yId
 
-# append a vector of ones to x0 to account for the bias
+# 
 X = np.concatenate(  (np.ones((x0.shape[0],1) ),x0) ,axis=1)
 
 print('X is \n',X,'\n')
@@ -56,9 +47,6 @@ print('w is \n',w,'\n')
 # GRADIENT DESCENT    
 for i in range(0,10000):
     
-    w0old = w0
-    w1old = w1
-
     w = w + alpha*np.dot(X.T, y - np.dot(X,w))
     
 
